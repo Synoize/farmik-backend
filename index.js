ae56cc30ca8dc7775fd9e7e674d3f9b24342b87f
synoize <suryavanshivam@gamil.com>
2025-05-19 12:12:51 +0530

    update

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ app.use(bodyParser.json())
 app.use(express.json())
 app.use(express.urlencoded({extended : true}))
 
+app.get('/', (req, res) => res.send("Server is now active."));
 // User routes
 app.use('/api/auth', authRoutes)
 app.use('/api/products',verifyToken, productsRoutes)
